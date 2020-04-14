9b4a3cd9d25710bede80d351dc782095dd6281b6
Lavine Oluoch <lavineoluoch@gmail.com>
2020-04-14 09:24:05 -0400

    Implement FUSE

--- affmount.py
+++ affmount.py
@@ -5,9 +5,13 @@ from pyaff4 import aff4_image
 from pyaff4 import lexicon
 from pyaff4 import rdfvalue
 from pyaff4 import zip
+from fuse import FUSE, FuseOSError, Operations
 
 import re
 import sys
+import os
+import errno
+
 
 def extractStream(path):
 	# Convert a filename to URN. AFF4 uses URNs to refer to everything. file to be mounted
@@ -39,20 +43,64 @@ def extractStream(path):
 				filename = re.sub("[^a-z0-9A-Z-]",
 								  lambda m: "%%%02x" % ord(m[0]),
 								  str(subject))
-				print ("Dumping %s to file %s" % (subject, filename))
+				print ("Writing %s to file %s" % (subject, filename))
 
-				with open(filename, "wb") as out_fd:
+				while 1:
+					data = in_fd.read(1024 * 1024)
+					if not data:
+						break
+				return data.decode('utf-8')
 
-					# Just copy the output to the file.
-					while 1:
-						data = in_fd.read(1024 * 1024)
-						if not data:
-							break
-						out_fd.write(data)
 
+class Passthrough(Operations):
+	def __init__(self,root):
+		self.root = root
+		
+	
+    def _full_path(self, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root, partial)
+        return path
+		
+	# Filesystem methods
+    # ==================
+
+    def getattr(self, path, fh=None):
+        full_path = self._full_path(path)
+        st = os.lstat(full_path)
+        return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
+                     'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
+
+    def readdir(self, path, fh):
+        full_path = self._full_path(path)
+
+        dirents = ['.', '..']
+        if os.path.isdir(full_path):
+            dirents.extend(os.listdir(full_path))
+        for r in dirents:
+            yield r
+
+   
+
+    # File methods
+    # ============
+
+    def read(self, path, length, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.read(fh, length)
+		
+		
 #Program execution starts here
-def main(): 
-    if len(sys.argv) < 2:
+def main(mountpoint, root):
+	if len(sys.argv) < 2:
         sys.exit("No file provided.")
+    elif len(sys.argv) < 3:
+        sys.exit("No mount directory specified.")
     else:
-        extractStream(sys.argv[1])       
+		imagestream = extractStream(root)
+		FUSE(Passthrough(imagestream), mountpoint,nothreads=True, foreground=True)
+
+
+if __name__ == '__main__':
+    main(sys.argv[2], sys.argv[1])
